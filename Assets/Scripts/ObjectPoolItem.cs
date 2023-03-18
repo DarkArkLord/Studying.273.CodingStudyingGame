462454d96c3d8d1ed755427b55cd140170ea5e54
DarkLord <dfhrhfan12@yandex.ru>
2023-03-19 01:06:23 +0400

    Add pool

---
+++ Assets/Scripts/ObjectPoolItem.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    internal class ObjectPoolItem
+    {
+        public bool IsFree { get; set; }
+        public GameObject Item { get; set; }
+
+        public ObjectPoolItem(GameObject item)
+        {
+            IsFree = true;
+            Item = item;
+        }
+    }
+}
---
+++ Assets/Scripts/ObjertPoolManager.cs
@@ -0,0 +1,78 @@
+using Assets.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjertPoolManager : MonoBehaviour
+{
+    public GameObject Prefab;
+    public int StartCount = 10;
+
+    private List<ObjectPoolItem> items;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        items = new List<ObjectPoolItem>(StartCount);
+        for (int i = 0; i < StartCount; i++)
+        {
+            AddItem();
+        }
+    }
+
+    private ObjectPoolItem AddItem()
+    {
+        var obj = Instantiate(Prefab);
+        obj.SetActive(false);
+        var item = new ObjectPoolItem(obj);
+        items.Add(item);
+        return item;
+    }
+
+    public IEnumerable<GameObject> GetObjects(int count)
+    {
+        var findedCount = 0;
+        foreach (var obj in items)
+        {
+            if (obj.IsFree)
+            {
+                obj.IsFree = false;
+                findedCount++;
+                yield return obj.Item;
+
+                if (findedCount >= count)
+                {
+                    yield break;
+                }
+            }
+        }
+
+        if (findedCount < count)
+        {
+            for (int i = findedCount; i < count; i++)
+            {
+                var item = AddItem();
+                item.IsFree = false;
+                yield return item.Item;
+            }
+        }
+    }
+
+    public void FreeObjects(IEnumerable<GameObject> objects)
+    {
+        foreach (var obj in objects)
+        {
+            var id = obj.GetInstanceID();
+            var item = items.Find(x => x.Item.GetInstanceID() == id);
+            if(item != null)
+            {
+                item.IsFree = true;
+                item.Item.SetActive(false);
+            }
+            else
+            {
+                Debug.LogError($"No object with id {id} for freeing");
+            }
+        }
+    }
+}
