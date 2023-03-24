b113e545d4b15c2ff4c58aab4bcd2b281e1d64f4
DarkLord <dfhrhfan12@yandex.ru>
2023-03-24 22:26:19 +0400

    Add MoveController

--- Assets/Scripts/JumpController.cs
+++ Assets/Scripts/JumpController.cs
@@ -1,40 +1,20 @@
 using UnityEngine;
 
-public class JumpController : MonoBehaviour
+public class JumpController : MoveController
 {
     public float JumpHeight { get; set; } = 0.5f;
-    public float JumpTime { get; set; } = 0.25f;
-
-    private Vector3 target;
-    private float progress;
     private float sqrt_h;
-    private Vector3 offset;
-    private Vector3 pathPoint;
 
-    public void SetTarget(Vector3 target)
+    public new void SetTarget(Vector3 target)
     {
-        this.target = target;
-        progress = 0;
+        base.SetTarget(target);
         sqrt_h = Mathf.Sqrt(JumpHeight);
-        pathPoint = transform.position;
-        offset = target - pathPoint;
     }
 
-    public bool Move()
+    private float HeightOffset => JumpHeight - ((Progress * 2 - 1) * sqrt_h) * ((Progress * 2 - 1) * sqrt_h);
+
+    protected new Vector3 GetStepPosition()
     {
-        var framePath = Time.deltaTime / JumpTime;
-        progress += framePath;
-
-        if (progress >= 1)
-        {
-            transform.position = target;
-            return false;
-        }
-
-        pathPoint += offset * framePath;
-        transform.position = pathPoint + Vector3.up * HeightOffset;
-        return true;
+        return base.GetStepPosition() + Vector3.up * HeightOffset;
     }
-
-    private float HeightOffset => JumpHeight - ((progress * 2 - 1) * sqrt_h) * ((progress * 2 - 1) * sqrt_h);
 }
---
+++ Assets/Scripts/MoveController.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class MoveController : MonoBehaviour
+{
+    public float MoveTime { get; set; } = 0.25f;
+
+    public Vector3 Target { get; private set; }
+    public float Progress { get; private set; }
+    public Vector3 MoveDirection { get; private set; }
+    public Vector3 PathPoint { get; private set; }
+
+    public virtual void SetTarget(Vector3 target)
+    {
+        Target = target;
+        Progress = 0;
+        PathPoint = transform.position;
+        MoveDirection = target - PathPoint;
+    }
+
+    protected float FramePart => Time.deltaTime / MoveTime;
+    protected Vector3 StepOffset => MoveDirection * FramePart;
+    protected virtual Vector3 GetStepPosition() => PathPoint;
+
+    public bool Move()
+    {
+        Progress += FramePart;
+
+        if (Progress >= 1)
+        {
+            transform.position = Target;
+            return false;
+        }
+
+        PathPoint += StepOffset;
+        transform.position = GetStepPosition();
+        return true;
+    }
+}
