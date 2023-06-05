2ed896ff24c1c452afbf4267098e58b7e8d07579
DarkLord <dfhrhfan12@yandex.ru>
2023-06-06 01:11:36 +0400

    Add map generation

---
+++ Assets/Scripts/NPCController.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPCController : MonoBehaviour
+{
+    public ObjertPoolManager ObjectPool;
+    public Material NpcMaterial;
+    public MapController Map;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
