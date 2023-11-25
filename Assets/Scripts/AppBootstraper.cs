fc0e911a5b213252987ae60a33dc1e50b4fafb12
DarkLord <dfhrhfan12@yandex.ru>
2023-11-25 21:13:34 +0400

    Upd bootstrap

--- Assets/Scripts/AppBootstraper.cs
+++ Assets/Scripts/AppBootstraper.cs
@@ -1,18 +1,21 @@
 using Assets.Scripts.Controllers;
+using Assets.Scripts.StatesMachine;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class AppBootstraper : MonoBehaviour
 {
+    private MainStatesController mainStatesController;
+
     private void Awake()
     {
-        //
+        mainStatesController = new MainStatesController();
     }
 
     // Update is called once per frame
     void Update()
     {
-        //
+        mainStatesController.Update();
     }
 }
