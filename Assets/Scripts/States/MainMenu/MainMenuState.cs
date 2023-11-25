8a4f3def42b39d6534086a66ca8ae99789e7e157
DarkLord <dfhrhfan12@yandex.ru>
2023-11-25 21:12:29 +0400

    Add main menu state

---
+++ Assets/Scripts/States/MainMenu/MainMenuState.cs
@@ -0,0 +1,17 @@
+﻿using Assets.Scripts.StatesMachine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assets.Scripts.States.MainMenu
+{
+    public class MainMenuState : BaseState
+    {
+        public override void Update()
+        {
+            //
+        }
+    }
+}
