6cf27caf7739e4951705a521ae396fc54b8d1be3
DarkLord <dfhrhfan12@yandex.ru>
2023-11-25 20:56:49 +0400

    Add base state controller

---
+++ Assets/Scripts/AppBootstraper.cs
@@ -0,0 +1,18 @@
+using Assets.Scripts.Controllers;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppBootstraper : MonoBehaviour
+{
+    private void Awake()
+    {
+        //
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //
+    }
+}
---
+++ Assets/Scripts/StatesMachine/BaseState.cs
@@ -0,0 +1,34 @@
+﻿namespace Assets.Scripts.StatesMachine
+{
+    public abstract class BaseState
+    {
+        protected MainStatesController MainController { get; private set; }
+
+        public void SetMainController(MainStatesController mainController)
+        {
+            MainController = mainController;
+        }
+
+        public virtual void OnStateCreating()
+        {
+            // Вызывается перед push
+        }
+
+        public virtual void OnStatePop()
+        {
+            // Вызывается после pop, если этот стейт станет current
+        }
+
+        public virtual void OnStatePush()
+        {
+            // Вызывается перед push, если этот стейт current
+        }
+
+        public virtual void OnStateDestroy()
+        {
+            // Вызывается после pop, если этот стейт current
+        }
+
+        public abstract void Update();
+    }
+}
