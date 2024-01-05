bbe64a733ee9dec5684253fbd0218e1e373c0c04
DarkLord <dfhrhfan12@yandex.ru>
2024-01-05 14:00:02 +0400

    Add other buttons

--- Assets/Scripts/States/MainMenu/MainMenuState.cs
+++ Assets/Scripts/States/MainMenu/MainMenuState.cs
@@ -24,27 +24,55 @@ namespace Assets.Scripts.States.MainMenu
         public override IEnumerator OnStateCreating()
         {
             yield return Ui.ShowPanelCorutine();
+
+            Ui.StartButton.OnClick.AddListener(StartButtonClick);
+            Ui.ContinueButton.OnClick.AddListener(ContinueButtonClick);
             Ui.ExitButton.OnClick.AddListener(ExitButtonClick);
+
+            if (controller.UsingStates.Count < 1)
+            {
+                Ui.ContinueButton.gameObject.SetActive(false);
+            }
+
             yield return base.OnStateCreating();
         }
 
         public override IEnumerator OnStatePush()
         {
             yield return Ui.HidePanelCorutine();
+
+            //
+
             yield return base.OnStatePush();
         }
 
         public override IEnumerator OnStatePop()
         {
             yield return Ui.ShowPanelCorutine();
+
+            //
+
             yield return base.OnStatePop();
         }
 
         public override IEnumerator OnStateDestroy()
         {
+            Ui.StartButton.OnClick.RemoveAllListeners();
+            Ui.ContinueButton.OnClick.RemoveAllListeners();
             Ui.ExitButton.OnClick.RemoveAllListeners();
+
             yield return Ui.HidePanelCorutine();
             yield return base.OnStateDestroy();
+        }
+
+        private void StartButtonClick()
+        {
+            controller.UseState(MainStateCode.Exit);
+        }
+
+        private void ContinueButtonClick()
+        {
+            controller.UseState(MainStateCode.Exit);
         }
 
         private void ExitButtonClick()
--- Assets/Scripts/States/MainMenu/MainMenuUi.cs
+++ Assets/Scripts/States/MainMenu/MainMenuUi.cs
@@ -10,6 +10,14 @@ namespace Assets.Scripts.States.MainMenu
         public CanvasRenderer BackgroundPanel => _backgroundPanel;
 
         [SerializeField]
+        private ButtonComponent _startButton;
+        public ButtonComponent StartButton => _startButton;
+
+        [SerializeField]
+        private ButtonComponent _continueButton;
+        public ButtonComponent ContinueButton => _continueButton;
+
+        [SerializeField]
         private ButtonComponent _exitButton;
         public ButtonComponent ExitButton => _exitButton;
     }
