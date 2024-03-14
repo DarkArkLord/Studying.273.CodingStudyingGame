4ad9064c47142e068d3a888f1a86b097507e9ace
DarkLord <dfhrhfan12@yandex.ru>
2024-03-14 22:47:00 +0400

    Upd parts count

--- Assets/Scripts/States/Map/Components/Generators/BaseMapGenerator.cs
+++ Assets/Scripts/States/Map/Components/Generators/BaseMapGenerator.cs
@@ -5,15 +5,17 @@ namespace Assets.Scripts.States.Map.Components.Generators
 {
     public abstract class BaseMapGenerator : MonoBehaviour, IMapGenerator
     {
+        private const double PointPartsCount = 5;
+
         protected Vector2Int GenerateInputPoint(System.Random random, int width, int height)
         {
-            var xPart = width / 10.0;
+            var xPart = width / PointPartsCount;
             var xBorderLeft = 0;
             var xBorderRight = (int)Math.Ceiling(xPart) + 1;
             var x = random.Next(xBorderLeft, xBorderRight);
 
-            var yPart = height / 10.0;
-            var yBorderTop = (int)Math.Floor(yPart * 9);
+            var yPart = height / PointPartsCount;
+            var yBorderTop = (int)Math.Floor(yPart * (PointPartsCount - 1));
             var yBorderDown = height;
             var y = random.Next(yBorderTop, yBorderDown);
 
@@ -22,12 +24,12 @@ namespace Assets.Scripts.States.Map.Components.Generators
 
         protected Vector2Int GenerateOutputPoint(System.Random random, int width, int height)
         {
-            var xPart = width / 10.0;
-            var xBorderLeft = (int)Math.Floor(xPart * 9);
+            var xPart = width / PointPartsCount;
+            var xBorderLeft = (int)Math.Floor(xPart * (PointPartsCount - 1));
             var xBorderRight = width;
             var x = random.Next(xBorderLeft, xBorderRight);
 
-            var yPart = height / 10.0;
+            var yPart = height / PointPartsCount;
             var yBorderTop = 0;
             var yBorderDown = (int)Math.Ceiling(yPart) + 1;
             var y = random.Next(yBorderTop, yBorderDown);
