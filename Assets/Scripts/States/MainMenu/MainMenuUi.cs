87cef444db0b2505e821917662e73da0693d202f
DarkLord <dfhrhfan12@yandex.ru>
2024-03-17 20:04:17 +0400

    Remove SerializeField

--- Assets/Scripts/States/MainMenu/MainMenuUi.cs
+++ Assets/Scripts/States/MainMenu/MainMenuUi.cs
@@ -6,10 +6,6 @@ namespace Assets.Scripts.States.MainMenu
     public class MainMenuUi : BaseUiModel
     {
         [SerializeField]
-        private CanvasRenderer _backgroundPanel;
-        public CanvasRenderer BackgroundPanel => _backgroundPanel;
-
-        [SerializeField]
         private ButtonComponent _startButton;
         public ButtonComponent StartButton => _startButton;
 
