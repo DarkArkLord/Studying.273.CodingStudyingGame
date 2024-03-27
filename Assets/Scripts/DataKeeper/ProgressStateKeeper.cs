5c80c0ac9027f32d9b9f825963dc7870df660fc8
DarkLord <dfhrhfan12@yandex.ru>
2024-03-27 22:19:01 +0400

    Add quests data

--- Assets/Scripts/DataKeeper/ProgressStateKeeper.cs
+++ Assets/Scripts/DataKeeper/ProgressStateKeeper.cs
@@ -7,9 +7,14 @@ namespace Assets.Scripts.DataKeeper
         public ProgressStateKeeper()
         {
             KilledEmeniesCounter = 0;
+
+            QuestsInfo = new QuestsInfoKeeper();
         }
 
         [JsonProperty]
         public int KilledEmeniesCounter { get; set; }
+
+        [JsonProperty]
+        public QuestsInfoKeeper QuestsInfo {  get; set; }
     }
 }
--- Assets/Scripts/DataKeeper/QuestsInfoKeeper.cs
+++ Assets/Scripts/DataKeeper/QuestsInfoKeeper.cs
@@ -1,16 +1,32 @@
-﻿using System;
+﻿using Assets.Scripts.DataKeeper.QuestsSystem;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Assets.Scripts.DataKeeper
 {
+    [JsonObject]
     public class QuestsInfoKeeper
     {
         public QuestsInfoKeeper()
         {
-            //
+            QuestsForStarting = new List<QuestIdEnum>();
+            QuestsForCompleting = new List<QuestIdEnum>();
+
+            QuestStates = new Dictionary<QuestIdEnum, QuestState>();
+            QuestProgress = new Dictionary<QuestIdEnum, int>();
+
+            foreach (QuestIdEnum state in Enum.GetValues(typeof(QuestIdEnum)).Cast<QuestIdEnum>())
+            {
+                QuestStates.Add(state, QuestState.NotAvailable);
+                QuestProgress.Add(state, 0);
+            }
         }
+
+        public List<QuestIdEnum> QuestsForStarting { get; set; }
+        public List<QuestIdEnum> QuestsForCompleting { get; set; }
+        public Dictionary<QuestIdEnum, QuestState> QuestStates { get; set; }
+        public Dictionary<QuestIdEnum, int> QuestProgress { get; set; }
     }
 }
