81a50d6aed7fcbfa4fda1142bc45c548267a06e1
DarkLord <dfhrhfan12@yandex.ru>
2024-04-30 18:24:10 +0400

    Upd quests list

--- Assets/Scripts/DataKeeper/QuestsSystem/Enums.cs
+++ Assets/Scripts/DataKeeper/QuestsSystem/Enums.cs
@@ -2,10 +2,15 @@
 {
     public enum QuestIdEnum
     {
-        Q1_GatherFlowers = 0,
-        Q2_HealFriends = 1,
-        Q3_KillEnemies = 2,
-        Q4_KillWolfes2 = 3,
+        Q1_F1_GatherFlowers = 0,
+        Q2_F1_HealFriends = 1,
+        Q3_F1_KillWolfes = 2,
+
+        Q4_F2_GatherFlowers = 3,
+        Q5_F2_HealFriends = 4,
+        Q6_F2_KillWolfes = 5,
+
+        Q7_F3_KillWolfes = 6,
     }
 
     public enum QuestState
